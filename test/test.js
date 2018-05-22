eee4fe217dc92ab7f53d2b5ee67bd6498f4d9c48
Michael Rawlings <mirawlings@ebay.com>
2018-05-22 14:41:42 -0700

    fix older versions of marko (#18)

--- test/test.js
+++ test/test.js
@@ -4,10 +4,16 @@ chai.Assertion.includeStack = true;
 require('chai').should();
 var expect = require('chai').expect;
 var nodePath = require('path');
+var semver = require('semver');
 var fs = require('fs');
 
 var markoPlugin = require('../'); // Load this module just to make sure it works
 var lasso = require('lasso');
+var lassoVersion = require('lasso/package').version;
+var markoVersion = require('marko/package').version;
+var isPackageTypesSupported = semver.satisfies(lassoVersion, '>=3') && semver.satisfies(markoVersion, '>=4.7');
+var isMetaSupported = semver.satisfies(markoVersion, '>=4');
+var isBrowserJSONInMeta = semver.satisfies(markoVersion, '>=4.4.9');
 
 var config = {
     fileWriter: {
@@ -34,42 +40,78 @@ describe('lasso-marko' , function() {
         done();
     });
 
-    it('should bundle a simple marko dependency tree', function() {
+    it('should bundle a simple marko template', function() {
         var myLasso = lasso.create(config);
 
         return myLasso.lassoPage({
-            name: 'basic',
+            name: 'simple',
             dependencies: [
-                nodePath.join(__dirname, 'fixtures/project1/simple.marko')
+                nodePath.join(__dirname, 'fixtures/simple/template.marko')
             ],
-            from: nodePath.join(__dirname, 'fixtures/project1')
+            from: nodePath.join(__dirname, 'fixtures/simple')
         }).then((lassoPageResult) => {
-            var JS = fs.readFileSync(nodePath.join(__dirname, 'static/basic.js'), {encoding: 'utf8'});
-            var CSS = fs.readFileSync(nodePath.join(__dirname, 'static/basic.css'), {encoding: 'utf8'});
-            expect(JS).to.contain("simple.marko");
+            var JS = fs.readFileSync(nodePath.join(__dirname, 'static/simple.js'), {encoding: 'utf8'});
             expect(JS).to.contain("input.name");
-            expect(JS).to.contain("TEST");
+            return lasso.flushAllCaches();
+        });
+    });
+
+    it('should bundle a simple marko template loaded using require', function() {
+        var myLasso = lasso.create(config);
+
+        return myLasso.lassoPage({
+            name: 'require',
+            dependencies: [
+               'require: ./template.marko'
+            ],
+            from: nodePath.join(__dirname, 'fixtures/simple')
+        }).then((lassoPageResult) => {
+            var JS = fs.readFileSync(nodePath.join(__dirname, 'static/require.js'), {encoding: 'utf8'});
+            expect(JS).to.contain("input.name");
+            return lasso.flushAllCaches();
+        });
+    });
+
+    it('should bundle a marko dependency tree with meta data', function() {
+        if (!isMetaSupported) return this.skip();
+
+        var myLasso = lasso.create(config);
+
+        return myLasso.lassoPage({
+            name: 'simple-meta',
+            dependencies: [
+                nodePath.join(__dirname, 'fixtures/meta-stateful/stateful.marko')
+            ],
+            from: nodePath.join(__dirname, 'fixtures/meta-stateful')
+        }).then((lassoPageResult) => {
+            var JS = fs.readFileSync(nodePath.join(__dirname, 'static/simple-meta.js'), {encoding: 'utf8'});
+            var CSS = fs.readFileSync(nodePath.join(__dirname, 'static/simple-meta.css'), {encoding: 'utf8'});
+            expect(JS).to.contain("stateful.marko");
+            expect(JS).to.contain("input.name");
+            if (isBrowserJSONInMeta) expect(JS).to.contain("TEST");
             expect(CSS).to.contain("blue");
             expect(CSS).to.contain("red");
             return lasso.flushAllCaches();
         });
     });
 
-    it('should bundle a simple marko dependency tree that uses require', function() {
+    it('should bundle a marko dependency tree with meta data that uses require', function() {
+        if (!isMetaSupported) return this.skip();
+        
         var myLasso = lasso.create(config);
 
         return myLasso.lassoPage({
-            name: 'require',
+            name: 'require-meta',
             dependencies: [
-                'require: ./simple.marko'
+                'require: ./stateful.marko'
             ],
-            from: nodePath.join(__dirname, 'fixtures/project1')
+            from: nodePath.join(__dirname, 'fixtures/meta-stateful')
         }).then((lassoPageResult) => {
-            var JS = fs.readFileSync(nodePath.join(__dirname, 'static/require.js'), {encoding: 'utf8'});
-            var CSS = fs.readFileSync(nodePath.join(__dirname, 'static/require.css'), {encoding: 'utf8'});
-            expect(JS).to.contain("simple.marko");
+            var JS = fs.readFileSync(nodePath.join(__dirname, 'static/require-meta.js'), {encoding: 'utf8'});
+            var CSS = fs.readFileSync(nodePath.join(__dirname, 'static/require-meta.css'), {encoding: 'utf8'});
+            expect(JS).to.contain("stateful.marko");
             expect(JS).to.contain("input.name");
-            expect(JS).to.contain("TEST");
+            if (isBrowserJSONInMeta) expect(JS).to.contain("TEST");
             expect(CSS).to.contain("blue");
             expect(CSS).to.contain("red");
             return lasso.flushAllCaches();
@@ -77,20 +119,20 @@ describe('lasso-marko' , function() {
     });
 
     it('should bundle a simple marko dependency tree that uses dependencies', function() {
-        if (require('lasso/package').version[0] < 3) this.skip();
+        if (!isPackageTypesSupported) return this.skip();
         
         var myLasso = lasso.create(config);
 
         return myLasso.lassoPage({
             name: 'dependencies',
             dependencies: [
-                'marko-dependencies: ./simple.marko'
+                'marko-dependencies: ./stateful.marko'
             ],
-            from: nodePath.join(__dirname, 'fixtures/project1')
+            from: nodePath.join(__dirname, 'fixtures/meta-stateful')
         }).then((lassoPageResult) => {
             var JS = fs.readFileSync(nodePath.join(__dirname, 'static/dependencies.js'), {encoding: 'utf8'});
             var CSS = fs.readFileSync(nodePath.join(__dirname, 'static/dependencies.css'), {encoding: 'utf8'});
-            expect(JS).to.not.contain("simple.marko");
+            expect(JS).to.not.contain("stateful.marko");
             expect(JS).to.not.contain("input.name");
             expect(JS).to.contain("TEST");
             expect(CSS).to.contain("blue");
@@ -100,20 +142,20 @@ describe('lasso-marko' , function() {
     });
 
     it('should bundle a simple marko dependency tree that uses hydrate', function() {
-        if (require('lasso/package').version[0] < 3) this.skip();
+        if (!isPackageTypesSupported) return this.skip();
 
         var myLasso = lasso.create(config);
 
         return myLasso.lassoPage({
             name: 'hydrate',
             dependencies: [
-                'marko-hydrate: ./simple.marko'
+                'marko-hydrate: ./stateful.marko'
             ],
-            from: nodePath.join(__dirname, 'fixtures/project1')
+            from: nodePath.join(__dirname, 'fixtures/meta-stateful')
         }).then((lassoPageResult) => {
             var JS = fs.readFileSync(nodePath.join(__dirname, 'static/hydrate.js'), {encoding: 'utf8'});
             var CSS = fs.readFileSync(nodePath.join(__dirname, 'static/hydrate.css'), {encoding: 'utf8'});
-            expect(JS).to.not.contain("simple.marko");
+            expect(JS).to.not.contain("stateful.marko");
             expect(JS).to.not.contain("input.name");
             expect(JS).to.contain("TEST");
             expect(CSS).to.contain("blue");
@@ -123,7 +165,7 @@ describe('lasso-marko' , function() {
     });
 
     it('should bundle a split component with its dependencies', function() {
-        if (require('lasso/package').version[0] < 3) this.skip();
+        if (!isPackageTypesSupported) return this.skip();
 
         var myLasso = lasso.create(config);
 
@@ -132,7 +174,7 @@ describe('lasso-marko' , function() {
             dependencies: [
                 'marko-dependencies: ./split.marko'
             ],
-            from: nodePath.join(__dirname, 'fixtures/project2')
+            from: nodePath.join(__dirname, 'fixtures/meta-split')
         }).then((lassoPageResult) => {
             var JS = fs.readFileSync(nodePath.join(__dirname, 'static/dependencies.js'), {encoding: 'utf8'});
             var CSS = fs.readFileSync(nodePath.join(__dirname, 'static/dependencies.css'), {encoding: 'utf8'});
